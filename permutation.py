8e26b1ac2eaede27038d7c029553283fd180bcfa
sahildeepphysic <sahildeepphysic@gmail.com>
2020-05-14 11:00:57 +0530

    String is a permutation of other string.

--- permutation.py
+++ permutation.py
@@ -2,4 +2,4 @@ def permutation(string1, string2):
 	if len(string1)==len(string2) and set(string1.lower())==set(string2.lower()):
 		return True
 	return False
-print(permutation("dog", "GoD"))
+permutation("dog", "GoDD")
